9a6e0a0e54aeecd964d3fc6eff7526bb91289138
Unisogdi <younes.sogandi@edu.dsti.institute>
2026-03-22 12:37:37 +0100

    Create variables.tf for project_id and region

---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "project_id" {
+  description = "The ID of the project to use"
+  type        = string
+}
+
+variable "region" {
+  description = "The region where resources will be created"
+  type        = string
+}
